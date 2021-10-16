793a04ae87c92ee49e89cb51f43fd1bdd09d6804
iwatamac <hallofzv164@gmail.com>
2021-10-16 16:39:00 +0900

    InputFieldのグリッド

--- src/components/MessageInputField.js
+++ src/components/MessageInputField.js
@@ -1,15 +1,25 @@
 import * as React from 'react';
 import {makeStyles} from '@mui/styles';
+import Grid from '@mui/material/Grid';
 
 const useStyles = makeStyles({
   root: {
     gridRow: 2,
+    margin: '26px'
   }
 })
 
 const MessageInputField = ({name}) => {
   const classes= useStyles();
-  return <div className={classes.root}>MessageInputField</div>;
+  return (
+  <div className={classes.root}>
+    <Grid container>
+      <Grid xs={1}>アイコン</Grid>
+      <Grid xs={10}>入力</Grid>
+      <Grid xs={1}>ボタン</Grid>
+    </Grid>
+    </div>
+  );
 };
 
 export default MessageInputField;
