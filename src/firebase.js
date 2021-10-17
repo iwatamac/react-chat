2f3ba9bf856b2adbaab4a744c0aa6048189575ba
iwatamac <hallofzv164@gmail.com>
2021-10-17 21:04:53 +0900

    remove firebase

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,22 @@
 import firebase from 'firebase/compat';
 /* firebaseのverによってcompatを記載した */
 
+const {
+REACT_APP_FIREBASE_API_KEY,
+REACT_APP_FIREBASE_AUTH_DOMAIN,
+REACT_APP_FIREBASE_PROJECT_ID,
+REACT_APP_FIREBASE_STORAGE_BUCKET,
+REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+REACT_APP_FIREBASE_APP_ID,
+} = process.env;
+
 const firebaseConfig = {
-    apiKey: "AIzaSyBvNzNEdtrlKviU3L4e2EF6vgIPwtLu1Nc",
-    authDomain: "react-chat-8c714.firebaseapp.com",
-    projectId: "react-chat-8c714",
-    storageBucket: "react-chat-8c714.appspot.com",
-    messagingSenderId: "52689947324",
-    appId: "1:52689947324:web:125ea4ecd3dc882b395eb9"
+    apiKey: REACT_APP_FIREBASE_API_KEY,
+    authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: REACT_APP_FIREBASE_APP_ID,
   };
 
  /* initialize(firebaseConfig); */
