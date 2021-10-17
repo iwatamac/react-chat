e31793df1a17a41d8da922015ca959a64ec1dc30
iwatamac <hallofzv164@gmail.com>
2021-10-17 17:59:35 +0900

    Gridにitemを設定

--- src/firebase.js
+++ src/firebase.js
@@ -14,8 +14,8 @@ const firebaseConfig = {
  /* initialize(firebaseConfig); */
   firebase.initializeApp(firebaseConfig);
   const database = firebase.database();
-  const messageRef = database.ref('messages')
+  const messagesRef = database.ref('messages')
 
   export const pushMessage = ({name, text}) => {
-    messageRef.push({name, text});
+    messagesRef.push({name, text});
   };
